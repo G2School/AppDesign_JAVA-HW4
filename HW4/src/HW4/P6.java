6f12e6e85782c96c4b26bf430725d55b25479c79
GinoTu <gino.tu130@gmail.com>
2022-12-01 11:55:15 +0800

    P6

--- HW4/src/HW4/P6.java
+++ HW4/src/HW4/P6.java
@@ -1,28 +1,30 @@
 package HW4;
 
-public class P4 
+public class P6 
 {
 	public static void main(String[] args)
 	{
-		RacingCarp4 rccar1;
-		rccar1 = new RacingCarp4();
-		
-		rccar1.setCar(1234,20.5);
-		
-		rccar1.setCourse(5);
+		RacingCarp6 rccar1 = new RacingCarp6(1234, 20.5, 5);
 	}
 }
 
-class Carp4
+class Carp6
 {
 	private int num;
 	private double gas;
 	
-	public Carp4()
+	public Carp6()
 	{
 		num = 0;
 		gas = 0.0;
 		System.out.println("生產了車子");
+	}
+	
+	public Carp6(int n, double g)
+	{
+		num = n;
+		gas = g;
+		System.out.println("生產了車號為 " + num + " 汽油量為 " + gas + " 的車子");
 	}
 	
 	public void setCar(int n, double g)
@@ -39,14 +41,21 @@ class Carp4
 	}
 }
 
-class RacingCarp4 extends Carp4
+class RacingCarp6 extends Carp6
 {
 	private int course;
 	
-	public RacingCarp4()
+	public RacingCarp6()
 	{
 		course = 0;
 		System.out.println("生產了賽車");
+	}
+	
+	public RacingCarp6(int n, double g, int c)
+	{
+		super(n, g);
+		course = c;
+		System.out.println("生產了編號為 " + course + " 的賽車");
 	}
 	
 	public void setCourse(int c)
@@ -55,12 +64,6 @@ class RacingCarp4 extends Carp4
 		System.out.println("將賽車編號設為 " + course);
 	}
 }
-
-
-
-
-
-
 
 
 
